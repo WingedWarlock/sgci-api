1992ae825bbf4d85094887e46c1397a0ccb344ca
Fábio Augusto Pigosso Nunes <fabio.wise@castgrouppartner.com.br>
2026-02-28 17:35:35 -0300

    Test: Implementação dos Testes Unitários para a classe PessoaManager.java

--- sgci/src/main/java/br/com/sgci/controller/schema/PessoaMapper.java
+++ sgci/src/main/java/br/com/sgci/controller/schema/PessoaMapper.java
@@ -11,6 +11,7 @@ public interface PessoaMapper {
 	PessoaMapper INSTANCE = Mappers.getMapper(PessoaMapper.class);
 	
 	@Mapping(source = "enderecoResponse", target = "endereco")
+	@Mapping(source = "pessoa.id", target = "id")
 	PessoaResponse toPessoaResponse(Pessoa pessoa, EnderecoResponse enderecoResponse);
 
 }
